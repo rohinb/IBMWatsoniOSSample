a0daae2b3e665370605d35a0357496d6c081f096
rohinb <rohinbhushan@gmail.com>
2017-03-16 21:41:27 -0700

    Initial commit

---
+++ IBMWatsonSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBMWatsonSample
+//
+//  Created by Rohin Bhushan on 3/16/17.
+//  Copyright © 2017 DreamTeam5Ever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
