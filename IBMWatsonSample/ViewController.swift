fe538212b036a5ecc2377d90ea1ecda0376e5294
Sahand Edrisian <sahandedrisian@Sahands-MacBook-Pro.local>
2017-03-18 03:11:29 -0700

    UI

--- IBMWatsonSample/ViewController.swift
+++ IBMWatsonSample/ViewController.swift
@@ -12,12 +12,14 @@ import AlchemyLanguageV1
 import SpeechToTextV1
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate {
-
+    
     @IBOutlet weak var startRecordingButton: UIButton!
     @IBOutlet weak var recordingTableView: UITableView!
     @IBOutlet weak var recordingTextView: UITextView!
     @IBOutlet weak var containerView: UIView!
     var blurView = UIVisualEffectView()
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var noteTypeSegment: UISegmentedControl!
     
     let sampleCell = ["Swift is your god now, obey the true almighty meme lord","Tuesday at 4:30 pm","14:05"]
     
@@ -29,13 +31,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var recordedLines = 0
     
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         sttSetup()
-//        tableViewGradient()
+        //        tableViewGradient()
         designSetup()
-	}
+    }
     
     func sttSetup() {
         // Hackpassword1!
@@ -94,15 +96,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             sender.setTitle("Stop Recording", for: .normal)
             sender.tag = 1
             addBlurView()
-            recordingTextView.alpha = 1
-        } else {
+            recordingTextView.animateAlpha(1, t: 0.3)
+        } else if sender.tag == 1 {
             sender.setTitle("Start Recording", for: .normal)
             sender.tag = 0
             removeBlurView()
-            recordingTextView.alpha = 0
+            recordingTextView.animateAlpha(0, t: 0.3)
+            popupView.animateAlpha(1, t: 0.3)
+        } else if sender.tag == 2 {
             
         }
-        moveTextViewUp()
+
     }
     
     func textViewDidChange(_ textView: UITextView) {
@@ -128,23 +132,50 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return numberOfLines
     }
     
-    // Text View Stuff
+    // MARK: Text View Stuff
     
     func moveTextViewUp() {
-        UIView.animate(withDuration: 0.3) { 
+        UIView.animate(withDuration: 0.3) {
             self.recordingTextView.frame.size.height += self.recordingTextView.font!.lineHeight
             self.recordingTextView.frame.origin.y -= self.recordingTextView.font!.lineHeight
         }
     }
     
+    // MARK: Popup Stuff
     
-
+    @IBAction func doneButtonClicked(_ sender: Any) {
+        
+        if noteTypeSegment.selectedSegmentIndex == 0 {
+            
+        } else {
+            let type = noteTypeSegment.selectedSegmentIndex * 4
+            recordingTextView.text = createNote(type: type)
+            changeTextView()
+            popupView.animateAlpha(0, t: 0.3)
+        }
+        
+    }
+    
+    func createNote(type: Int) -> String {
+        
+        return ""
+    }
+    
+    func changeTextView() {
+        recordingTextView.isEditable = false
+        recordingTextView.isSelectable = true
+        recordingTextView.frame.origin.y = 35
+        recordingTextView.isScrollEnabled = true
+        
+    }
+    
+    
     // MARK: Table View Stuff
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = recordingTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! RecordingTableViewCell
         
@@ -162,7 +193,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
-
+    
 }
 
 class RecordingTableViewCell: UITableViewCell {
@@ -178,7 +209,13 @@ class RecordingTableViewCell: UITableViewCell {
     
 }
 
-
+extension UIView {
+    func animateAlpha(_ a: CGFloat, t: Double) {
+        UIView.animate(withDuration: t, animations: {
+            self.alpha = a
+        })
+    }
+}
 
 
 
