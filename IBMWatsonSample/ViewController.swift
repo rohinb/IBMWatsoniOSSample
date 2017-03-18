4956b7db2b6e739cf9f52f00e9439e245fe06a7f
Sahand Edrisian <sahandedrisian@Sahands-MacBook-Pro.local>
2017-03-17 23:39:56 -0700

    textview

--- IBMWatsonSample/ViewController.swift
+++ IBMWatsonSample/ViewController.swift
@@ -11,19 +11,29 @@ import NaturalLanguageClassifierV1
 import AlchemyLanguageV1
 import SpeechToTextV1
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate {
 
     @IBOutlet weak var startRecordingButton: UIButton!
     @IBOutlet weak var recordingTableView: UITableView!
-    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var recordingTextView: UITextView!
+    @IBOutlet weak var containerView: UIView!
+    var blurView = UIVisualEffectView()
     
     let sampleCell = ["Swift is your god now, obey the true almighty meme lord","Tuesday at 4:30 pm","14:05"]
+    
+    var currentText = "" {
+        didSet {
+            recordingTextView.text = currentText
+        }
+    }
+    
+    var recordedLines = 0
     
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
         sttSetup()
-        tableViewGradient()
+//        tableViewGradient()
         designSetup()
 	}
     
@@ -36,6 +46,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let settings = RecognitionSettings(contentType: .basic)
         stt.recognizeMicrophone(settings: settings) { (res) in
             print(res)
+            self.currentText = res.bestTranscript
         }
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
@@ -46,6 +57,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func designSetup() {
         startRecordingButton.layer.cornerRadius = 5.0
+        recordingTextView.alpha = 0
     }
     
     func tableViewGradient() {
@@ -58,21 +70,72 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         recordingTableView.superview?.layer.mask = gradient
     }
     
+    func addBlurView() {
+        blurView = UIVisualEffectView()
+        blurView.frame = view.bounds
+        let blurEffect = UIBlurEffect(style: .light)
+        containerView.addSubview(blurView)
+        UIView.animate(withDuration: 0.3) {
+            self.blurView.effect = blurEffect
+        }
+    }
+    
+    func removeBlurView() {
+        UIView.animate(withDuration: 0.3) {
+            self.blurView.effect = nil
+        }
+    }
+    
     // MARK: Recording
     
     @IBAction func startRecordingClicked(_ sender: UIButton) {
         
-        let blurEffect = UIBlurEffect(style: .dark)
-        blurView.effect = blurEffect
         if sender.tag == 0 {
             sender.setTitle("Stop Recording", for: .normal)
             sender.tag = 1
+            addBlurView()
+            recordingTextView.alpha = 1
         } else {
             sender.setTitle("Start Recording", for: .normal)
-            blurView.effect = nil
+            sender.tag = 0
+            removeBlurView()
+            recordingTextView.alpha = 0
+            
+        }
+        moveTextViewUp()
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let lines = numberOfLines(textView: textView)
+        if lines > recordedLines {
+            recordedLines = lines
+            moveTextViewUp()
         }
     }
     
+    func numberOfLines(textView: UITextView) -> Int {
+        let layoutManager = textView.layoutManager
+        let numberOfGlyphs = layoutManager.numberOfGlyphs
+        var lineRange: NSRange = NSMakeRange(0, 1)
+        var index = 0
+        var numberOfLines = 0
+        
+        while index < numberOfGlyphs {
+            layoutManager.lineFragmentRect(forGlyphAt: index, effectiveRange: &lineRange)
+            index = NSMaxRange(lineRange)
+            numberOfLines += 1
+        }
+        return numberOfLines
+    }
+    
+    // Text View Stuff
+    
+    func moveTextViewUp() {
+        UIView.animate(withDuration: 0.3) { 
+            self.recordingTextView.frame.size.height += self.recordingTextView.font!.lineHeight
+            self.recordingTextView.frame.origin.y -= self.recordingTextView.font!.lineHeight
+        }
+    }
     
     
 
